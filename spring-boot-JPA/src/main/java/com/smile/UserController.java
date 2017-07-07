1b658f78a0f7ae4f61ea63275eda4a1ace89b1eb
RiteshChavhan <rites.chavan@gmail.com>
2017-07-07 12:31:47 +0530

    Update UserController.java

--- spring-boot-JPA/src/main/java/com/smile/UserController.java
+++ spring-boot-JPA/src/main/java/com/smile/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author rushikeshM
+ * @author ritesh
  *
  */
 @RestController
